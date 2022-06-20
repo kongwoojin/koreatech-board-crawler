006f703e88c2c12dfd3a8b0ef61444b3335afafd
kongwoojin <kongwoojin03@gmail.com>
2022-06-20 20:15:31 +0900

    If user try read page that not exist, return status: END

--- crawler.py
+++ crawler.py
@@ -12,11 +12,14 @@ async def cse_parser(url: str):
         soup = BeautifulSoup(html, 'html.parser')
         posts = soup.select("#board_list > table > tbody > tr")
         for post in posts:
-            num = post.select_one("td:nth-child(1)").get_text().strip()
-            title = post.select_one("td.title > a").get_text().strip()
-            writer = post.select_one("td.author").get_text().strip()
-            write_date = post.select_one("td.time").get_text().strip()
-            read = post.select_one("td.readNum").get_text().strip()
+            try:
+                num = post.select_one("td:nth-child(1)").get_text().strip()
+                title = post.select_one("td.title > a").get_text().strip()
+                writer = post.select_one("td.author").get_text().strip()
+                write_date = post.select_one("td.time").get_text().strip()
+                read = post.select_one("td.readNum").get_text().strip()
+            except AttributeError as e:
+                return jsonable_encoder([{"status": "END"}])
 
             data_dic = {
                 'num': num,
