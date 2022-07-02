6e83d066dddac5d6c212bbf9b1a22b6f7b89a3c5
kongwoojin <kongwoojin03@gmail.com>
2022-07-02 23:38:11 +0900

    Rename def

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ async def get_cse_notice(params: dict = Depends(cse_notice)):
 
 
 @app.get("/cse/job/")
-async def get_cse_job(params: dict = Depends(cse_job_board)):
+async def get_cse_job_board(params: dict = Depends(cse_job_board)):
     return params
 
 
 @app.get("/cse/free/")
-async def get_cse_free(params: dict = Depends(cse_free_board)):
+async def get_cse_free_board(params: dict = Depends(cse_free_board)):
     return params
 
 
@@ -26,25 +26,25 @@ async def get_cse_article(params: dict = Depends(cse_article_parser)):
 
 
 @app.get("/school/notice/")
-async def get_school_notice(params: dict = Depends(school_general_notice)):
+async def get_school_general_notice(params: dict = Depends(school_general_notice)):
     return params
 
 
 @app.get("/school/scholar/")
-async def get_school_notice(params: dict = Depends(school_scholar_notice)):
+async def get_school_scholar_notice(params: dict = Depends(school_scholar_notice)):
     return params
 
 
 @app.get("/school/bachelor/")
-async def get_school_notice(params: dict = Depends(school_bachelor_notice)):
+async def get_school_bachelor_notice(params: dict = Depends(school_bachelor_notice)):
     return params
 
 
 @app.get("/school/covid19/")
-async def get_school_notice(params: dict = Depends(school_covid19_notice)):
+async def get_school_covid19_notice(params: dict = Depends(school_covid19_notice)):
     return params
 
 
 @app.get("/school/article/")
-async def get_school_notice(params: dict = Depends(school_article_parser)):
+async def get_school_article(params: dict = Depends(school_article_parser)):
     return params
