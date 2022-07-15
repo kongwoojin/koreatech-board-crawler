ac198f799a939f1a055c72de3d433177ddfb8cd9
kongwoojin <kongwoojin03@gmail.com>
2022-07-15 20:02:58 +0900

    Add all departments with their own websites

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI
 from cse_crawler import *
 from school_crawler import *
 from dorm_crawler import *
+from department_common_crawler import *
 
 app = FastAPI()
 
@@ -23,6 +24,116 @@ async def get_cse_free_board(params: dict = Depends(cse_free_board)):
 
 @app.get("/cse/article/")
 async def get_cse_article(params: dict = Depends(cse_article_parser)):
+    return params
+
+
+@app.get("/mechanical/notice/")
+async def get_mechanical_notice(params: dict = Depends(mechanical_notice)):
+    return params
+
+
+@app.get("/mechanical/lecture/")
+async def get_mechanical_lecture_notice(params: dict = Depends(mechanical_lecture_notice)):
+    return params
+
+
+@app.get("/mechanical/free/")
+async def get_mechanical_free_board(params: dict = Depends(mechanical_free_board)):
+    return params
+
+
+@app.get("/mechanical/article/")
+async def get_mechanical_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@app.get("/mechatronics/notice/")
+async def get_mechanical_notice(params: dict = Depends(mechatronics_notice)):
+    return params
+
+
+@app.get("/mechatronics/lecture/")
+async def get_mechanical_lecture_notice(params: dict = Depends(mechatronics_lecture_notice)):
+    return params
+
+
+@app.get("/mechatronics/bachelor/")
+async def get_mechanical_bachelor_notice(params: dict = Depends(mechatronics_bachelor_notice)):
+    return params
+
+
+@app.get("/mechatronics/job/")
+async def get_mechanical_job_notice(params: dict = Depends(mechatronics_job_notice)):
+    return params
+
+
+@app.get("/mechatronics/free/")
+async def get_mechanical_free_board(params: dict = Depends(mechatronics_free_board)):
+    return params
+
+
+@app.get("/mechatronics/article/")
+async def get_mechanical_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@app.get("/ide/notice/")
+async def get_ite_notice(params: dict = Depends(ite_notice)):
+    return params
+
+
+@app.get("/ite/article/")
+async def get_ite_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@app.get("/ide/notice/")
+async def get_ide_notice(params: dict = Depends(ide_notice)):
+    return params
+
+
+@app.get("/ide/free/")
+async def get_ide_free_board(params: dict = Depends(ide_free_board)):
+    return params
+
+
+@app.get("/ide/article/")
+async def get_ide_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@app.get("/arch/notice/")
+async def get_arch_notice(params: dict = Depends(arch_notice)):
+    return params
+
+
+@app.get("/arch/free/")
+async def get_arch_free_board(params: dict = Depends(arch_free_board)):
+    return params
+
+
+@app.get("/arch/article/")
+async def get_arch_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@app.get("/emc/notice/")
+async def get_emc_notice(params: dict = Depends(emc_notice)):
+    return params
+
+
+@app.get("/emc/article/")
+async def get_emc_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@app.get("/sim/notice/")
+async def get_sim_notice(params: dict = Depends(sim_notice)):
+    return params
+
+
+@app.get("/sim/article/")
+async def get_sim_article(params: dict = Depends(department_common_article_parser)):
     return params
 
 
