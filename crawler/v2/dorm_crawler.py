1f7f6be789b40bc3751ce6e2e40c55f5468be23f
kongwoojin <kongwoojin03@gmail.com>
2022-09-03 21:32:18 +0900

    Ready for refactor

---
+++ crawler/v2/dorm_crawler.py
@@ -0,0 +1,113 @@
+import re
+
+import requests
+from bs4 import BeautifulSoup
+from fastapi.encoders import jsonable_encoder
+
+
+async def dorm_article_parser(url: str):
+    response = requests.get(url, verify=False)
+
+    if response.status_code == 200:
+        data_list = []
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        try:
+            title = soup.select_one(
+                "#board > div.boardViewer > h4").get_text().strip()
+            writer = soup.select_one(
+                "#board > div.boardViewer > table.viewer tr:nth-child(2) > td:nth-child(2)").get_text().strip()
+            text = soup.select_one(
+                "#board > div.boardViewer > table.viewer div.story").decode_contents()
+            date = soup.select_one(
+                "#board > div.boardViewer > table.viewer tr:nth-child(2) > td:nth-child(4)").get_text().strip()
+
+        except AttributeError as e:
+            return jsonable_encoder([{"status": "END"}])
+
+        text = text.replace("<img", "<br><img")
+
+        files = soup.select("#board > div.boardViewer > table.viewer tr:nth-child(3) > td > a")
+
+        file_list = []
+        for file in files:
+            file_uri = file["href"]
+            file_name = file.get_text()
+            file_name = re.sub("\[.*]", "", file_name).strip()
+
+            file_dic = {
+                "file_uri": f"https://dorm.koreatech.ac.kr{file_uri}",
+                "file_name": file_name
+            }
+
+            file_list.append(file_dic)
+
+        data_dic = {
+            'title': title,
+            'writer': writer,
+            'text': text,
+            'date': date,
+            'files': file_list
+        }
+        data_list.append(data_dic)
+
+        return jsonable_encoder(data_list)
+    else:
+        return jsonable_encoder({'status_code': response.status_code})
+
+
+async def dorm_parser(board: str, page: int, is_second_page: bool = False):
+    if not is_second_page:
+        page = page * 2 - 1
+
+    url = f"https://dorm.koreatech.ac.kr/content/board/list.php?now_page={page}&GUBN=&SEARCH=&BOARDID={board}"
+    response = requests.get(url, verify=False)
+
+    if response.status_code == 200:
+        data_list = []
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        posts = soup.select("#board > table > tbody > tr")
+        for post in posts:
+            try:
+                num = post.select_one("td:nth-child(1)").get_text().strip()
+                title = post.select_one("td:nth-child(2)").get_text().strip()
+                writer = post.select_one("td:nth-child(3)").get_text().strip()
+                write_date = post.select_one("td:nth-child(4)").get_text().strip()
+                if board == "notice":
+                    read = post.select_one("td:nth-child(6)").get_text().strip()
+                else:
+                    read = post.select_one("td:nth-child(5)").get_text().strip()
+                article_url = post.select_one("td:nth-child(2) > a").get('href')
+            except AttributeError as e:
+                return jsonable_encoder([{"status": "END"}])
+
+            data_dic = {
+                'num': num,
+                'title': title,
+                'writer': writer,
+                'write_date': write_date,
+                'read': read,
+                'article_url': f"https://dorm.koreatech.ac.kr/content/board/{article_url}"
+            }
+
+            data_list.append(data_dic)
+
+        if page % 2 == 1:
+            page += 1
+            data_list.extend(await dorm_parser(board, page, True))
+
+        if is_second_page:
+            return data_list
+        else:
+            return jsonable_encoder(data_list)
+    else:
+        return jsonable_encoder({'status_code': response.status_code})
+
+
+async def dorm_notice(page: int = 1):
+    return await dorm_parser("notice", page)
+
+
+async def dorm_free_board(page: int = 1):
+    return await dorm_parser("bulletin", page)
---
+++ routers/v1/api.py
@@ -0,0 +1,180 @@
+from fastapi import APIRouter, Depends
+from crawler.v1.cse_crawler import *
+from crawler.v1.school_crawler import *
+from crawler.v1.dorm_crawler import *
+from crawler.v1.department_common_crawler import *
+
+router = APIRouter(
+    tags=["legacy"],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/cse/notice/")
+async def get_cse_notice(params: dict = Depends(cse_notice)):
+    return params
+
+
+@router.get("/cse/job/")
+async def get_cse_job_board(params: dict = Depends(cse_job_board)):
+    return params
+
+
+@router.get("/cse/free/")
+async def get_cse_free_board(params: dict = Depends(cse_free_board)):
+    return params
+
+
+@router.get("/cse/article/")
+async def get_cse_article(params: dict = Depends(cse_article_parser)):
+    return params
+
+
+@router.get("/mechanical/notice/")
+async def get_mechanical_notice(params: dict = Depends(mechanical_notice)):
+    return params
+
+
+@router.get("/mechanical/lecture/")
+async def get_mechanical_lecture_notice(params: dict = Depends(mechanical_lecture_notice)):
+    return params
+
+
+@router.get("/mechanical/free/")
+async def get_mechanical_free_board(params: dict = Depends(mechanical_free_board)):
+    return params
+
+
+@router.get("/mechanical/article/")
+async def get_mechanical_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/mechatronics/notice/")
+async def get_mechanical_notice(params: dict = Depends(mechatronics_notice)):
+    return params
+
+
+@router.get("/mechatronics/lecture/")
+async def get_mechanical_lecture_notice(params: dict = Depends(mechatronics_lecture_notice)):
+    return params
+
+
+@router.get("/mechatronics/bachelor/")
+async def get_mechanical_bachelor_notice(params: dict = Depends(mechatronics_bachelor_notice)):
+    return params
+
+
+@router.get("/mechatronics/job/")
+async def get_mechanical_job_notice(params: dict = Depends(mechatronics_job_notice)):
+    return params
+
+
+@router.get("/mechatronics/free/")
+async def get_mechanical_free_board(params: dict = Depends(mechatronics_free_board)):
+    return params
+
+
+@router.get("/mechatronics/article/")
+async def get_mechanical_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/ite/notice/")
+async def get_ite_notice(params: dict = Depends(ite_notice)):
+    return params
+
+
+@router.get("/ite/article/")
+async def get_ite_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/ide/notice/")
+async def get_ide_notice(params: dict = Depends(ide_notice)):
+    return params
+
+
+@router.get("/ide/free/")
+async def get_ide_free_board(params: dict = Depends(ide_free_board)):
+    return params
+
+
+@router.get("/ide/article/")
+async def get_ide_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/arch/notice/")
+async def get_arch_notice(params: dict = Depends(arch_notice)):
+    return params
+
+
+@router.get("/arch/free/")
+async def get_arch_free_board(params: dict = Depends(arch_free_board)):
+    return params
+
+
+@router.get("/arch/article/")
+async def get_arch_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/emc/notice/")
+async def get_emc_notice(params: dict = Depends(emc_notice)):
+    return params
+
+
+@router.get("/emc/article/")
+async def get_emc_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/sim/notice/")
+async def get_sim_notice(params: dict = Depends(sim_notice)):
+    return params
+
+
+@router.get("/sim/article/")
+async def get_sim_article(params: dict = Depends(department_common_article_parser)):
+    return params
+
+
+@router.get("/school/notice/")
+async def get_school_general_notice(params: dict = Depends(school_general_notice)):
+    return params
+
+
+@router.get("/school/scholar/")
+async def get_school_scholar_notice(params: dict = Depends(school_scholar_notice)):
+    return params
+
+
+@router.get("/school/bachelor/")
+async def get_school_bachelor_notice(params: dict = Depends(school_bachelor_notice)):
+    return params
+
+
+@router.get("/school/covid19/")
+async def get_school_covid19_notice(params: dict = Depends(school_covid19_notice)):
+    return params
+
+
+@router.get("/school/article/")
+async def get_school_article(params: dict = Depends(school_article_parser)):
+    return params
+
+
+@router.get("/dorm/notice/")
+async def get_dorm_notice(params: dict = Depends(dorm_notice)):
+    return params
+
+
+@router.get("/dorm/free/")
+async def get_dorm_free_board(params: dict = Depends(dorm_free_board)):
+    return params
+
+
+@router.get("/dorm/article/")
+async def get_dorm_article(params: dict = Depends(dorm_article_parser)):
+    return params
