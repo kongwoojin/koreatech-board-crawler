836203cab9c2ca2204641a403c195640501f9739
kongwoojin <kongwoojin03@gmail.com>
2023-05-16 01:54:29 +0900

    feat: Add crawling scheduler

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from apscheduler.schedulers.background import BackgroundScheduler
+
 from app.routers.v1 import api
 # from app.routers.v2 import cse, sim, dorm, school, mechatronics, emc, ide, arch, mechanical, ite
 from app.routers.v3 import cse, sim, dorm, school, mechatronics, emc, ide, arch, mechanical, ite
+
+from app.crawler.v3.main import main_crawler_one, main_crawler_two
 
 app = FastAPI()
 
@@ -18,3 +22,13 @@ app.include_router(school.router)
 app.include_router(sim.router)
 
 app.include_router(api.router)
+
+
+@app.on_event('startup')
+def start_crawler():
+    sched = BackgroundScheduler()
+    sched.add_job(main_crawler_one, 'cron', hour="*", minute=0)
+    sched.add_job(main_crawler_two, 'cron', hour="*", minute=30)
+    sched.start()
+
+    print("Crawling started..")
