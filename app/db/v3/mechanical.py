111ee305d2656a0fa56cb665c7c29071c88e8934
kongwoojin <kongwoojin03@gmail.com>
2023-06-03 14:50:59 +0900

    chore: Remove article_url from article list api

--- app/db/v3/mechanical.py
+++ app/db/v3/mechanical.py
@@ -8,7 +8,7 @@ client = edgedb.create_async_client()
 async def get_data(board: str, page: int, num_of_items: int):
     data = await client.query("""
             select mechanical 
-            {num, title, writer, write_date, article_url} 
+            {num, title, writer, write_date} 
             filter .board=<str>$board order by contains(.num, '공지') desc 
             then .write_date desc
             then .num desc offset <int64>$offset limit <int64>$num_of_items
