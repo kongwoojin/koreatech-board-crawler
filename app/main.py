8e211bc38827a01a8ea47222d37d28ed59c7f450
WooJin Kong <kongwoojin03@gmail.com>
2023-09-06 10:46:01 +0900

    feat: Remove fastapi
    * Switch API server to https://github.com/kongwoojin/koreatech-board-api-golang

--- app/main.py
+++ app/main.py
@@ -1,30 +1,14 @@
-import asyncio
-from multiprocessing import cpu_count
+from rocketry import Rocketry
+from rocketry.conds import every
+from app.crawler.v3.main import main_crawler
 
-import uvicorn
-import sys
-from os import path
-
-sys.path.append(path.abspath('.'))
-
-from api import app as app_fastapi
-from scheduler import app as app_rocketry
+app = Rocketry(execution="async")
 
 
-class Server(uvicorn.Server):
-    def handle_exit(self, sig: int, frame) -> None:
-        app_rocketry.session.shut_down()
-        return super().handle_exit(sig, frame)
-
-
-async def main():
-    server = Server(config=uvicorn.Config(app_fastapi, host="0.0.0.0", workers=cpu_count(), loop="asyncio"))
-
-    api = asyncio.create_task(server.serve())
-    sched = asyncio.create_task(app_rocketry.serve())
-
-    await asyncio.wait([sched, api])
+@app.task(every('3 hours', based="finish"))
+async def crawler():
+    await main_crawler()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    app.run()
