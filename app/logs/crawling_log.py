12e75e473558bc2f6703656622254776f1883595
WooJin Kong <kongwoojin03@gmail.com>
2023-09-15 10:57:04 +0900

    feat: Update logs format

--- app/logs/crawling_log.py
+++ app/logs/crawling_log.py
@@ -1,29 +1,34 @@
 import inspect
 import os.path
+import time
 
 from app.dataclass.board import Board
 
 
 def attribute_exception_error(url, exception):
     caller = os.path.splitext(os.path.basename(inspect.stack()[1].filename))[0]
-    print(f"{caller}: Failed to crawling {url} due to attribute error, {exception}")
+    print("{} {:<24}: Failed to crawling {} due to attribute error, {}".format(current_time(), caller, url, exception))
 
 
 def unknown_exception_error(exception):
     caller = os.path.splitext(os.path.basename(inspect.stack()[1].filename))[0]
-    print(f"{caller}: {exception}")
+    print("{} {:<24}: {}".format(current_time(), caller, exception))
 
 
 def http_response_error(status, url):
     caller = os.path.splitext(os.path.basename(inspect.stack()[1].filename))[0]
-    print(f"{caller}: {status} received when trying to crawling {url}")
+    print("{} {:<24}: {} received when trying to crawling {}".format(current_time(), caller, status, url))
 
 
 def article_crawling_log(data: Board):
     caller = os.path.splitext(os.path.basename(inspect.stack()[1].filename))[0]
-    print(f"{caller}: Start crawling article {data.num} from {data.board}...")
+    print("{} {:<24}: Start crawling article {} of {}".format(current_time(), caller, data.num, data.board))
 
 
 def board_crawling_log(board, page: int):
     caller = os.path.splitext(os.path.basename(inspect.stack()[1].filename))[0]
-    print(f"{caller}: Start crawling {board} page {page}...")
+    print("{} {:<24}: Start crawling page {} of {}".format(current_time(), caller, page, board))
+
+
+def current_time():
+    return time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
