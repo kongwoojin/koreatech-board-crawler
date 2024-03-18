1bda8de1862f4391c012df01c6e9d5c86e7e0d16
WooJin Kong <kongwoojin03@gmail.com>
2024-03-18 09:25:08 +0900

    fix: Remove page parameter from article url

--- app/crawler/v3/common_crawler.py
+++ app/crawler/v3/common_crawler.py
@@ -184,6 +184,7 @@ async def board_page_crawler(session, department: Department, board_index: int, 
                         else:
                             article_url_parsed = post.select_one("td.txt_left > a").get("href")
                         article_url_parsed = f"https://koreatech.ac.kr{article_url_parsed}"
+                        article_url_parsed = re.sub("&nPage=\d*", "", article_url_parsed)
                         write_date_parsed = post.select_one(
                             f"td:nth-child({len(post.find_all('td')) - 2})").text.strip()
                         write_date_parsed = datetime.strptime(write_date_parsed, '%Y.%m.%d')
