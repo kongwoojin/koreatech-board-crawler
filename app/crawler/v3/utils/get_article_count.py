72d13a778c5e76f4b5c24521199c0a1bab936d85
WooJin Kong <kongwoojin03@gmail.com>
2024-03-24 10:00:21 +0900

    fix: Fix typo

--- app/crawler/v3/utils/get_article_count.py
+++ app/crawler/v3/utils/get_article_count.py
@@ -3,15 +3,15 @@ from app.dataclass.enums.department import Department
 from app.db.v3 import edgedb_client
 
 
-def get_article_count(department: Department, board: Board) -> int:
+def get_article_count(department: Department, board: str) -> int:
     client = edgedb_client()
 
     count = client.query("SELECT count(notice filter .department=<Department><str>$department AND "
                          ".board=<Board><str>$board)",
-                         department=department.department, board=board.board)
+                         department=department.department, board=board)
 
     return int(count[0])
 
 
 if __name__ == '__main__':
-    print(get_article_count(Department.CSE, Board.NOTICE))
+    print(get_article_count(Department.CSE, Department.CSE.boards[0].board))
